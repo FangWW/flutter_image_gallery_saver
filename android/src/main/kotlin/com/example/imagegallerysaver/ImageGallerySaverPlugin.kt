4fd994791d653db7e8b6fe9b8c32191d60e90c88
FangJW <1065680448@qq.com>
2018-11-14 20:25:38 +0800

    Update ImageGallerySaverPlugin.kt

--- android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
+++ android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
@@ -18,32 +18,9 @@ class ImageGallerySaverPlugin(): MethodCallHandler {
   override fun onMethodCall(call: MethodCall, result: Result): Unit {
     if (call.method == "saveImageToGallery") {
       val image = call.arguments as ByteArray
-      result.success(saveImageToGallery(BitmapFactory.decodeByteArray(image,0,image.size)))
+      result.success(false)
     } else {
       result.notImplemented()
     }
-  }
-
-  private fun saveImageToGallery(bmp: Bitmap): Boolean {
-    val context = registrar.activeContext().applicationContext
-    val storePath =  Environment.getExternalStorageDirectory().absolutePath + File.separator + "image_saver_gallery"
-    val appDir = File(storePath)
-    if (!appDir.exists()) {
-      appDir.mkdir()
-    }
-    val fileName = System.currentTimeMillis().toString() + ".png"
-    val file = File(appDir, fileName)
-    try {
-      val fos = FileOutputStream(file)
-      val isSuccess = bmp.compress(Bitmap.CompressFormat.JPEG, 60, fos)
-      fos.flush()
-      fos.close()
-      val uri = Uri.fromFile(file)
-      context.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri))
-      return isSuccess
-    } catch (e: IOException) {
-      e.printStackTrace()
-    }
-    return false
   }
 }
